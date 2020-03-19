08d4169232f9761efe1a022aeeadf88500637231
Mathias Elmhauge Petersen <mathiasdk1@hotmail.dk>
2020-03-19 11:32:37 +0100

    En masse

--- src/js/index.ts
+++ src/js/index.ts
@@ -3,6 +3,9 @@ import axios, {
     AxiosError
 } from "../../node_modules/axios/index";
 
+import { json2table100 } from "./generictable";
+
+
 interface ICar {
     id: number;
     model: string;
@@ -23,18 +26,38 @@ buttonDeleteElement.addEventListener("click", deleteCar);
 let addButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("addButton");
 addButton.addEventListener("click", addCar);
 
+let updateButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("updateButton");
+updateButton.addEventListener("click", UpdateCar);
+
+let deleteDropdown: HTMLSelectElement = <HTMLSelectElement>document.getElementById("deleteSelect");
+
+
+
+
+
+
+
 function showAllCars(): void {
     axios.get<ICar[]>(baseUri)
         .then(function (response: AxiosResponse<ICar[]>): void {
             // element.innerHTML = generateSuccessHTMLOutput(response);
             // outputHtmlElement.innerHTML = generateHtmlTable(response.data);
             // outputHtmlElement.innerHTML = JSON.stringify(response.data);
-            let result: string = "<ul id='carlist'>";
-            response.data.forEach((car: ICar) => {
-                result += "<li>" + car.id + " " + car.model + " " + car.vendor + " "+ car.price +  "</li>";
-            });
-            result += "</ul>";
+            let result: string = json2table100(response.data);
             outputElement.innerHTML = result;
+
+            let OutputItems: HTMLScriptElement = <HTMLScriptElement>document.getElementById("deleteItems")
+
+
+            var newOption = document.createElement("option");
+          
+            
+            for (var car of response.data) {
+                var num = car.id;
+                newOption.text = num.toString();
+                newOption.value = car.id.toString();
+                deleteDropdown.appendChild(newOption);
+            }
         })
         .catch(function (error: AxiosError): void { // error in GET or in generateSuccess?
             if (error.response) {
@@ -46,6 +69,7 @@ function showAllCars(): void {
                 outputElement.innerHTML = error.message;
             }
         });
+    
 }
 
 function deleteCar(): void {
@@ -71,6 +95,45 @@ function deleteCar(): void {
             }
         });
 }
+
+
+
+
+
+function UpdateCar(): void {
+    let output: HTMLDivElement = <HTMLDivElement>document.getElementById("contentAdd");
+    let inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById("deleteInput");
+    let addModelElement: HTMLInputElement = <HTMLInputElement>document.getElementById("addModel");
+    let addVendorElement: HTMLInputElement = <HTMLInputElement>document.getElementById("addVendor");
+    let addPriceElement: HTMLInputElement = <HTMLInputElement>document.getElementById("addPrice");
+    let myModel: string = addModelElement.value;
+    let myVendor: string = addVendorElement.value;
+    let myPrice: number = Number(addPriceElement.value);
+
+    let id: string = inputElement.value;
+    let uri: string = baseUri + "/" + id;
+    axios.put<ICar>(uri, { model: myModel, vendor: myVendor, price: myPrice})
+
+        .then(function (response: AxiosResponse<ICar>): void { 
+            // element.innerHTML = generateSuccessHTMLOutput(response);
+            // outputHtmlElement.innerHTML = generateHtmlTable(response.data);
+            console.log(JSON.stringify(response));
+            output.innerHTML = response.status + " " + response.statusText;
+        })
+        .catch(function (error: AxiosError): void { // error in GET or in generateSuccess?
+            if (error.response) {
+                // the request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                // https://kapeli.com/cheat_sheets/Axios.docset/Contents/Resources/Documents/index
+                output.innerHTML = error.message;
+            } else { // something went wrong in the .then block?
+                output.innerHTML = error.message;
+            }
+        });
+}
+
+
+
 
 function addCar(): void {
     let addModelElement: HTMLInputElement = <HTMLInputElement>document.getElementById("addModel");
